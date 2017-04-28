912b8774aa55ece5bf9e5607b8c8d5a1213d577d
Sarah Mount <sarah.mount@kcl.ac.uk>
2017-04-28 11:04:55 +0100

    Run rustfmt on Travis. Exit with an error and diff if rustfmt fails. Reformat source code.

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,37 @@
 //
 // The Universal Permissive License (UPL), Version 1.0
 //
-// Subject to the condition set forth below, permission is hereby granted to any person obtaining a
-// copy of this software, associated documentation and/or data (collectively the "Software"), free
-// of charge and under any and all copyright rights in the Software, and any and all patent rights
-// owned or freely licensable by each licensor hereunder covering either (i) the unmodified
-// Software as contributed to or provided by such licensor, or (ii) the Larger Works (as defined
-// below), to deal in both
+// Subject to the condition set forth below, permission is hereby granted to any
+// person obtaining a copy of this software, associated documentation and/or
+// data (collectively the "Software"), free of charge and under any and all
+// copyright rights in the Software, and any and all patent rights owned or
+// freely licensable by each licensor hereunder covering either (i) the
+// unmodified Software as contributed to or provided by such licensor, or (ii)
+// the Larger Works (as defined below), to deal in both
 //
 // (a) the Software, and
-// (b) any piece of software and/or hardware listed in the lrgrwrks.txt file
-// if one is included with the Software (each a “Larger Work” to which the Software is contributed
-// by such licensors),
+// (b) any piece of software and/or hardware listed in the lrgrwrks.txt file //
+// if one is included with the Software (each a “Larger Work” to which the Software
+// is contributed by such licensors),
 //
-// without restriction, including without limitation the rights to copy, create derivative works
-// of, display, perform, and distribute the Software and make, use, sell, offer for sale, import,
-// export, have made, and have sold the Software and the Larger Work(s), and to sublicense the
-// foregoing rights on either these or other terms.
+// without restriction, including without limitation the rights to copy, create
+// derivative works of, display, perform, and distribute the Software and make,
+// use, sell, offer for sale, import, export, have made, and have sold the
+// Software and the Larger Work(s), and to sublicense the foregoing rights on
+// either these or other terms.
 //
-// This license is subject to the following condition: The above copyright notice and either this
-// complete permission notice or at a minimum a reference to the UPL must be included in all copies
-// or substantial portions of the Software.
+// This license is subject to the following condition: The above copyright
+// notice and either this complete permission notice or at a minimum a reference
+// to the UPL must be included in all copies or substantial portions of the
+// Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #![feature(try_from)]
 
@@ -56,7 +61,7 @@ fn usage(prog: String, msg: &str) {
     let path = Path::new(prog.as_str());
     let leaf = match path.file_name() {
         Some(m) => m.to_str().unwrap(),
-        None => "rstreediff"
+        None => "rstreediff",
     };
     if msg.len() > 0 {
         writeln!(&mut stderr(), "{}", msg).ok();
@@ -81,11 +86,12 @@ fn read_file(path: &str) -> String {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let prog = args[0].clone();
-    let matches = match Options::new()
-                                .optflag("h", "help", "")
-                                .parse(&args[1..]) {
+    let matches = match Options::new().optflag("h", "help", "").parse(&args[1..]) {
         Ok(m) => m,
-        Err(f) => { usage(prog, f.to_string().as_str()); return }
+        Err(f) => {
+            usage(prog, f.to_string().as_str());
+            return;
+        }
     };
 
     if matches.opt_present("h") || matches.free.len() != 1 {
@@ -99,9 +105,12 @@ fn main() {
     let extension = match file_path.extension() {
         Some(ext) => ext.to_str().unwrap(),
         None => {
-            writeln!(&mut stderr(), "Cannot determine file type of {}.", &matches.free[0]).ok();
+            writeln!(&mut stderr(),
+                     "Cannot determine file type of {}.",
+                     &matches.free[0])
+                    .ok();
             return;
-        },
+        }
     };
     let lex_l_path = format!("grammars/{}.l", extension);
     let yacc_y_path = format!("grammars/{}.y", extension);
@@ -131,14 +140,17 @@ fn main() {
     // Sync up the IDs of terminals in the lexer and parser.
     let mut rule_ids = HashMap::<&str, u16>::new();
     for term_idx in grm.iter_term_idxs() {
-        rule_ids.insert(grm.term_name(term_idx).unwrap(), u16::try_from(usize::from(term_idx)).unwrap());
+        rule_ids.insert(grm.term_name(term_idx).unwrap(),
+                        u16::try_from(usize::from(term_idx)).unwrap());
     }
     lexer.set_rule_ids(&rule_ids);
 
     let input = read_file(&matches.free[0]);
 
     let mut lexemes = lexer.lex(&input).unwrap();
-    lexemes.push(Lexeme::new(u16::try_from(usize::from(grm.end_term)).unwrap(), input.len(), 0));
+    lexemes.push(Lexeme::new(u16::try_from(usize::from(grm.end_term)).unwrap(),
+                             input.len(),
+                             0));
     let pt = parse::<u16>(&grm, &stable, &lexemes).unwrap();
 
     // Print parse tree.
