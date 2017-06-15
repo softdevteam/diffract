6bcc91a11ae02006ccf9f9de1cb5aac9a3656b4c
Sarah Mount <sarah.mount@kcl.ac.uk>
2017-06-15 14:41:30 +0100

    Add line and column numbers to AST nodes.

--- src/lib/hqueue.rs
+++ src/lib/hqueue.rs
@@ -184,14 +184,44 @@ mod tests {
 
     fn create_arena() -> Arena<String> {
         let mut arena = Arena::new();
-        let root = arena.new_node(String::from("+"), String::from("Expr"), 0, None, None);
-        let n1 = arena.new_node(String::from("1"), String::from("INT"), 2, None, None);
+        let root = arena.new_node(String::from("+"),
+                                  String::from("Expr"),
+                                  0,
+                                  None,
+                                  None,
+                                  None,
+                                  None);
+        let n1 = arena.new_node(String::from("1"),
+                                String::from("INT"),
+                                2,
+                                None,
+                                None,
+                                None,
+                                None);
         n1.make_child_of(root, &mut arena).unwrap();
-        let n2 = arena.new_node(String::from("*"), String::from("Expr"), 2, None, None);
+        let n2 = arena.new_node(String::from("*"),
+                                String::from("Expr"),
+                                2,
+                                None,
+                                None,
+                                None,
+                                None);
         n2.make_child_of(root, &mut arena).unwrap();
-        let n3 = arena.new_node(String::from("3"), String::from("INT"), 4, None, None);
+        let n3 = arena.new_node(String::from("3"),
+                                String::from("INT"),
+                                4,
+                                None,
+                                None,
+                                None,
+                                None);
         n3.make_child_of(n2, &mut arena).unwrap();
-        let n4 = arena.new_node(String::from("4"), String::from("INT"), 4, None, None);
+        let n4 = arena.new_node(String::from("4"),
+                                String::from("INT"),
+                                4,
+                                None,
+                                None,
+                                None,
+                                None);
         n4.make_child_of(n2, &mut arena).unwrap();
         let format1 = "Expr +
   INT 1
@@ -332,7 +362,13 @@ mod tests {
     fn bench_push(bencher: &mut Bencher) {
         let mut arena: Arena<String> = Arena::new();
         for _ in 0..BENCH_ITER {
-            arena.new_node(String::from(""), String::from(""), 0, None, None);
+            arena.new_node(String::from(""),
+                           String::from(""),
+                           0,
+                           None,
+                           None,
+                           None,
+                           None);
         }
         let mut queue = HeightQueue::new();
         // Because `HeightQueues` are sets, each iteration of this
