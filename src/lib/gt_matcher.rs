b659067fbd662b732d35761f17e880feca705e6c
Sarah Mount <sarah.mount@kcl.ac.uk>
2018-09-04 14:06:29 +0100

    Most of greedy bottom-up matcher done. Debug code left in.

--- src/lib/gt_matcher.rs
+++ src/lib/gt_matcher.rs
@@ -40,6 +40,7 @@
 use std::fmt::Debug;
 
 use std::cmp::min;
+use std::collections::HashSet;
 
 use ast::{Arena, DstNodeId, NodeId, SrcNodeId};
 use hqueue::HeightQueue;
@@ -60,8 +61,8 @@ pub struct GumTreeConfig {
 
 impl Default for GumTreeConfig {
     fn default() -> GumTreeConfig {
-        GumTreeConfig { max_size: 100,
-                        min_dice: 0.3,
+        GumTreeConfig { max_size: 1000,
+                        min_dice: 0.3,  // FIXME Used for Jaccard?
                         min_height: 2 }
     }
 }
@@ -105,7 +106,7 @@ Code Differencing.";
 
 // Top-down algorithm to find isomorphic subtrees of decreasing height.
 fn get_gt_anchor_mappings<T: Clone + Debug + Eq + ToString + 'static>(store: &MappingStore<T>,
-                                                                     config: &GumTreeConfig) {
+                                                                      config: &GumTreeConfig) {
     let mut src_q: HeightQueue<SrcNodeId> = HeightQueue::new();
     let mut dst_q: HeightQueue<DstNodeId> = HeightQueue::new();
     let candidates = MappingStore::new(store.src_arena.borrow().clone(),
@@ -115,7 +116,8 @@ fn get_gt_anchor_mappings<T: Clone + Debug + Eq + ToString + 'static>(store: &Ma
     }
     src_q.push(NodeId::new(0), &store.src_arena.borrow());
     dst_q.push(NodeId::new(0), &store.dst_arena.borrow());
-    while !src_q.is_empty() && !dst_q.is_empty()
+    while !src_q.is_empty()
+          && !dst_q.is_empty()
           && min(src_q.peek_max().unwrap(), dst_q.peek_max().unwrap())
              > u32::from(config.min_height)
     {
@@ -150,9 +152,114 @@ fn get_gt_anchor_mappings<T: Clone + Debug + Eq + ToString + 'static>(store: &Ma
 // number of common anchors. When two nodes match, we apply an optimal algorithm
 // to search for additional (recovery) mappings among their descendants.
 fn get_gt_container_mappings<T: Clone + Debug + Eq + ToString + 'static>(store: &MappingStore<T>,
-                                                                    _config: &GumTreeConfig) {
+                                                                         config: &GumTreeConfig) {
     if store.src_arena.borrow().is_empty() || store.dst_arena.borrow().is_empty() {
         return;
     }
-    // FIXME
+    let mut _src_ids: HashSet<NodeId<SrcNodeId>> = HashSet::new(); // TreeMap in GT
+    let mut _dst_ids: HashSet<NodeId<DstNodeId>> = HashSet::new();
+    let src_root = store.src_arena.borrow().root().unwrap();
+    let dst_root = store.dst_arena.borrow().root().unwrap();
+    for node in src_root.post_order_traversal(&store.src_arena.borrow()) {
+        if node.is_root(&store.src_arena.borrow()) {
+            store.push(node, dst_root, &MappingType::CONTAINER);
+            last_chance_match(store, config, node, dst_root);
+            break;
+        } else if !(store.contains_src(node) || node.is_leaf(&store.src_arena.borrow())) {
+            let candidates = get_dst_candidates(store, config, node);
+            let mut best: Option<NodeId<DstNodeId>> = None;
+            let mut max: f64 = -1.0;
+            for candidate in candidates {
+                let sim = store.jaccard_sim(node, candidate);
+                if sim > max && sim >= f64::from(config.min_dice) {  // FIXME
+                    max = sim;
+                    best = Some(candidate);
+                }
+            }
+            if best.is_some() {
+                last_chance_match(store, config, node, best.unwrap());
+                store.push(node, best.unwrap(), &MappingType::CONTAINER);
+            }
+        }
+    }
+    // clean();  // FIXME DEFINITION IS:
+    // protected void clean() {
+    //     for (ITree t : src.getTrees())
+    //         if (!mappings.hasSrc(t))
+    //             t.setMatched(false);
+    //     for (ITree t : dst.getTrees())
+    //         if (!mappings.hasDst(t))
+    //             t.setMatched(false);
+    // }
+}
+
+fn get_dst_candidates<T: Clone + Debug + Eq + ToString + 'static>(store: &MappingStore<T>,
+                                                                  _config: &GumTreeConfig,
+                                                                  src: NodeId<SrcNodeId>)
+                                                                  -> Vec<NodeId<DstNodeId>> {
+    let mut seeds: Vec<NodeId<DstNodeId>> = vec![];
+    for child in src.descendants(&store.src_arena.borrow()) {
+        let mapped = store.get_dst(child);
+        if mapped.is_some() {
+            seeds.push(mapped.unwrap());
+        }
+    }
+    let mut candidates: Vec<NodeId<DstNodeId>> = vec![];
+    let mut visited: HashSet<NodeId<DstNodeId>> = HashSet::new();
+    for mut seed in seeds {
+        while seed.parent(&store.dst_arena.borrow()).is_some() {
+            let parent = seed.parent(&store.dst_arena.borrow()).unwrap();
+            if visited.contains(&parent) {
+                break;
+            }
+            visited.insert(parent);
+            if parent.ty(&store.dst_arena.borrow()) == src.ty(&store.src_arena.borrow())
+               && !store.contains_dst(parent)
+               && parent.is_root(&store.dst_arena.borrow())
+            {
+                candidates.push(parent);
+            }
+            seed = parent;
+        }
+    }
+    candidates
+}
+
+// GT comment suggests:
+// checks if it is better or not to remove the already found mappings.
+fn last_chance_match<T: Clone + Debug + Eq + ToString + 'static>(_store: &MappingStore<T>,
+                                                                 _config: &GumTreeConfig,
+                                                                 _src: NodeId<SrcNodeId>,
+                                                                 _dst: NodeId<DstNodeId>) {
+    // private void lastChanceMatch(ITree src, ITree dst) {
+    //     ITree cSrc = src.deepCopy();
+    //     ITree cDst = dst.deepCopy();
+    //     TreeUtils.removeMatched(cSrc);
+    //     TreeUtils.removeMatched(cDst);
+
+    //     if (cSrc.getSize() < SIZE_THRESHOLD || cDst.getSize() < SIZE_THRESHOLD) {
+    //         Matcher m = new ZsMatcher(cSrc, cDst, new MappingStore());
+    //         m.match();
+    //         for (Mapping candidate: m.getMappings()) {
+    //             ITree left = srcIds.getTree(candidate.getFirst().getId());
+    //             ITree right = dstIds.getTree(candidate.getSecond().getId());
+
+    //             if (left.getId() == src.getId() || right.getId() == dst.getId()) {
+    //                 //System.err.println("Trying to map already mapped source node.");
+    //                 continue;
+    //             } else if (!left.isMatchable(right)) {
+    //                 //System.err.println("Trying to map not compatible nodes.");
+    //                 continue;
+    //             } else if (left.getParent().getType() != right.getParent().getType()) {
+    //                 //System.err.println("Trying to map nodes with incompatible parents");
+    //                 continue;
+    //             } else addMapping(left, right);
+    //         }
+    //     }
+
+    //     for (ITree t : src.getTrees())
+    //         t.setMatched(true);
+    //     for (ITree t : dst.getTrees())
+    //         t.setMatched(true);
+    // }
 }
